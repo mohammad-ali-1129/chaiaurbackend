d3c7d05327d1e6145cdc9b7a2a0d4d8757fde1c6
Mohammad Ali <aliuser1129@gmail.com>
2026-03-26 23:35:25 +0530

    Learnt about import syntax for using express and added type module in package.json

---
+++ fullstack-basics/backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send("Server is ready")
+})
+
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+    console.log(`Server is listen at ${port}`);
+    
+})
